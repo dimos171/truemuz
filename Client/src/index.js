07f05a65f03091568643edaff026a787382488b6
Administrator <dimos171@gamil.com>
2020-06-07 12:30:50 +0300

    React - Azure B2C Auth

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
+import { AzureAD } from 'react-aad-msal';
 
 import App from './components/App';
 import './shared/globals.scss';
 
+import { authProvider } from './services/authProvider';
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('app')
+  <AzureAD provider={authProvider} forceLogin={true}>
+    <Router>
+      <App />
+    </Router>,
+  </AzureAD>,
+  document.getElementById('app'),
 );
 
 module.hot.accept();
---
+++ Client/src/services/authProvider.js
@@ -0,0 +1,31 @@
+import "regenerator-runtime/runtime.js";
+import { MsalAuthProvider, LoginType } from 'react-aad-msal';
+ 
+// Msal Configurations
+const config = {
+  auth: {
+    authority: 'https://truemuz.b2clogin.com/truemuz.onmicrosoft.com/B2C_1_spa-react',
+    clientId: '4696c1b9-b3b3-4b66-850e-4a8466ab7bc5',
+    validateAuthority: false,
+    redirectUri: window.location.origin
+  },
+  cache: {
+    cacheLocation: "localStorage",
+    storeAuthStateInCookie: true,
+  },
+};
+ 
+// Authentication Parameters
+const authenticationParameters = {
+  scopes: [
+    'https://truemuz.onmicrosoft.com/4696c1b9-b3b3-4b66-850e-4a8466ab7bc5/truemuz-scope',
+  ],
+};
+ 
+// Options
+const options = {
+  loginType: LoginType.Redirect,
+  //tokenRefreshUri: window.location.origin + '/auth.html',
+};
+ 
+export const authProvider = new MsalAuthProvider(config, authenticationParameters, options);
