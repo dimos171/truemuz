9b6db8f2a6ef4be42736a9e3a8ccafb80df4735e
Administrator <dimos171@gamil.com>
2020-06-07 22:58:19 +0300

    React - Login/Logout

--- Client/src/components/Header/index.js
+++ Client/src/components/Header/index.js
@@ -1,27 +1,68 @@
 import React from 'react';
-import { IoIosSearch } from "react-icons/io";
+import { IoIosSearch, IoMdLogIn, IoMdLogOut } from "react-icons/io";
+import { AzureAD, AuthenticationState } from 'react-aad-msal';
 
 import './index.scss';
+import { authProvider } from '../../services/authProvider';
 
 export default function Header() {
   return (
-    <header className="header-container px-5 d-none d-lg-block">
-      <div className="header-container-filler"></div>
-      <div className="header-container-content d-flex align-items-center">
-        <div className="col-5 p-0">
-          <h4>
-            TRUEMUZ
-          </h4>
-        </div>
-        <div className="col-2 p-0 text-center">
-          <h5>
-            MODERNOVA
-          </h5>
-        </div>
-        <div className="col-5 p-0 text-right">
-          <IoIosSearch className="icon" size="1.3em" />
+    <div>
+      <div className="auth">
+        <div className="col-md-5 col-sm-3 p-0">
+          <AzureAD provider={authProvider} forceLogin={false}>
+            {
+              ({ login, logout, authenticationState, error, accountInfo }) => {
+                switch (authenticationState) {
+                  case AuthenticationState.Authenticated:
+                    return (
+                      <h6>
+                        <span className="d-none d-lg-inline">Welcom, {accountInfo.account.name}!</span>
+                        <span className="d-none d-lg-inline icon ml-2" onClick={logout}>Logout </span>
+
+                        <div className="d-lg-none icon account-name text-truncate w-100" onClick={logout}>{accountInfo.account.name}</div>
+                        <span className="d-lg-none icon" onClick={logout}>Logout </span>
+                        
+                        <IoMdLogOut className="icon" onClick={logout}></IoMdLogOut>
+                      </h6>
+                    );
+                  case AuthenticationState.Unauthenticated:
+                    return (
+                      <div>
+                        {error && <p><span>An error occured during authentication, please try again!</span></p>}
+                        <h6>
+                          <span className="d-none d-lg-inline">Hey stranger, you look new!</span>
+                          <span className="icon ml-2" onClick={login}>Login </span>
+                          <IoMdLogIn className="icon" onClick={login}></IoMdLogIn>
+                        </h6>
+                      </div>
+                    );
+                  case AuthenticationState.InProgress:
+                    return (<p>Authenticating...</p>);
+                }
+              }
+            }
+          </AzureAD>
         </div>
       </div>
-    </header>
+      <header className="header-container px-5 d-none d-lg-block">
+        <div className="header-container-filler"></div>
+        <div className="header-container-content d-flex align-items-center">
+          <div className="col-5 p-0">
+            <h4>
+              TRUEMUZ
+          </h4>
+          </div>
+          <div className="col-2 p-0 text-center">
+            <h5>
+              MODERNOVA
+          </h5>
+          </div>
+          <div className="col-5 p-0 text-right">
+            <IoIosSearch className="icon" size="1.3em" />
+          </div>
+        </div>
+      </header>
+    </div>
   );
 }
--- Client/src/index.js
+++ Client/src/index.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
-import { AzureAD } from 'react-aad-msal';
 
 import App from './components/App';
 import './shared/globals.scss';
 
-import { authProvider } from './services/authProvider';
-
 ReactDOM.render(
-  <AzureAD provider={authProvider} forceLogin={true}>
-    <Router>
-      <App />
-    </Router>,
-  </AzureAD>,
-  document.getElementById('app'),
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('app')
 );
 
 module.hot.accept();
